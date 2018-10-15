b389a870b48871ae960791af822e428b1d08ae51
Virginia Dooley <vcdooley@gmail.com>
2018-10-15 16:02:54 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -1,18 +1,18 @@
 class Song 
   
-  attr_accessor :name
-  attr_accessor :artist
-  attr_accessor :genre 
+  attr_accessor :name, :artist, :genre 
   
   @@count = 0 
   @@artists = []
-  @@genres 
+  @@genres = []
   
   def initialize(name, artist, genre)
     @name = name
     @artist = artist 
     @genre = genre 
     @@count += 1
+    @@artists << artist 
+    @@genres << genre 
   end 
   
   def self.count
@@ -20,6 +20,7 @@ class Song
   end
   
   def self.artists
+    #check array 
   end 
   
   def self.genres
