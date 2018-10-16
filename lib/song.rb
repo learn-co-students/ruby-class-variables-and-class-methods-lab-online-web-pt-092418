20495f9d8f157512f74acab85361151c2c248f2d
Virginia Dooley <vcdooley@gmail.com>
2018-10-16 09:55:23 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -20,16 +20,35 @@ class Song
   end
   
   def self.artists
-    #check array 
+    @@artists.uniq
   end 
   
   def self.genres
+    @@genres.uniq 
   end 
   
   def self.genre_count
-  end 
+    genre_count = {} 
+    @@genres.each do |genre| 
+      if genre_count[genre] #if genre is already included, it skips to next below
+        genre_count[genre] += 1 
+      else
+        genre_count[genre] = 1 
+      end
+    end
+    genre_count
+  end
   
   def self.artist_count
+    artist_count = {} 
+    @@artists.each do |artist| 
+      if artist_count[artist] 
+        artist_count[artist] += 1 
+      else
+        artist_count[artist] = 1 
+      end
+    end
+    artist_count
   end
 
 end 
