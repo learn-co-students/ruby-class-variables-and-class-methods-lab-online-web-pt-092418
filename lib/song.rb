f11639e51fabbdc25d7ac1cc28307dde46f524ba
Kevin J Gomez <kjgmez1@gmail.com>
2018-10-16 22:11:20 +0000

    Done.

---
+++ lib/song.rb
@@ -0,0 +1,33 @@
+require 'pry'
+class Song
+  attr_reader :name, :artist, :genre
+
+  @@count = 0
+  @@genres = []
+  @@artists = []
+
+  def self.count
+    @@count
+  end
+  def self.genres
+    @@genres.uniq!
+  end
+  def self.artists
+    @@artists.uniq!
+  end
+  def self.genre_count
+    @@gen = Hash[@@genres.group_by {|x| x}.map{|k,v| [k,v.count]}]
+  end
+  def self.artist_count
+    @@art = Hash[@@artists.group_by {|x| x}.map{|k,v| [k,v.count]}]
+  end
+
+  def initialize (name, artist, genre)
+    @name = name
+    @artist = artist
+    @genre = genre
+    @@count += 1
+    @@genres << @genre
+    @@artists << @artist
+  end
+end
